934b1de6fc71829e284e5a40f32d6446b6eee5bc
omwaii <om.ghungarde@waiin.com>
2025-03-27 19:02:06 +0530

    gfhdfgh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BindingComponent } from './binding/binding.component';
+import { BindingComponent } from './01binding/binding.component';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { CommonModule } from '@angular/common'; // Import FormsModule
-import { Profilecomponent } from './Profile/profile.component';
+import { Profilecomponent } from './00Profile/profile.component';
 
-import { CounterComponent } from './counter/counter.component';
+import { CounterComponent } from './02counter/counter.component';
 import { log } from 'console';
-import { EventAngularComponent } from './event-angular/event-angular.component';
-import { GetsetComponent } from './getset/getset.component';
-import { IfElseComponent } from './if-else/if-else.component';
-import { PracticeMeComponent } from './practice-me/practice-me.component';
-import { LoopsComponent } from './loops/loops.component';
-import { SignalsAllComponent } from './signals-all/signals-all.component';
-import { LoopcontextualComponent } from "./loopcontextual/loopcontextual.component";
-import { TwowaybindingComponent } from './twowaybinding/twowaybinding.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { DynamicStyleComponent } from "./dynamic-style/dynamic-style.component";
+import { EventAngularComponent } from './07event-angular/event-angular.component';
+import { GetsetComponent } from './03getset/getset.component';
+import { IfElseComponent } from './04if-else/if-else.component';
+import { PracticeMeComponent } from './-1practice-me/practice-me.component';
+import { LoopsComponent } from './05loops/loops.component';
+import { SignalsAllComponent } from './06signals-all/signals-all.component';
+import { LoopcontextualComponent } from "./08loopcontextual/loopcontextual.component";
+import { TwowaybindingComponent } from './09twowaybinding/twowaybinding.component';
+import { TodoListComponent } from './10todo-list/todo-list.component';
+import { DynamicStyleComponent } from "./11dynamic-style/dynamic-style.component";
 @Component({
   selector: 'app-root',
   imports: [CommonModule, FormsModule, BindingComponent, RouterOutlet, Profilecomponent, CounterComponent, EventAngularComponent, GetsetComponent, IfElseComponent, PracticeMeComponent, LoopsComponent, SignalsAllComponent, LoopcontextualComponent, TwowaybindingComponent, TodoListComponent, DynamicStyleComponent],
